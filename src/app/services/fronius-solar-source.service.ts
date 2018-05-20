34219cbde905fad203dd15e818f41a042f401bc0
Libor Masek <libor.masek@gmail.com>
2018-05-20 12:07:11 +0800

    Added httpclient to fronius

--- src/app/services/fronius-solar-source.service.ts
+++ src/app/services/fronius-solar-source.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { SolarSourceService } from './solar-source.service';
 import { Observable } from 'rxjs-compat';
+import { HttpClient } from "@angular/common/http";
+
+const url: string = "test";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FroniusSolarSourceService implements SolarSourceService  {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
 
   currentPower(): number {
 
+    // TODO
+    //let test: Observable<string> = this.http.get<any>(url).map((result)=> {return result.Power});
     // TODO
     return Math.random();
   }
